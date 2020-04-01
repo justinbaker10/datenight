9882f5009ced71388804893ec01b710850bac223
Keilen Wiley <wiley9453@gmail.com>
2020-03-31 19:54:52 -0500

    drink generator button

---
+++ wiley-cocktail-api.js
@@ -0,0 +1,25 @@
+
+    const DrinkHeaders = new Headers({ 
+        'x-api-key': '1' 
+        }); 
+         
+        const DrinkBtn = document.getElementById("generateCocktailBtn") 
+        const CocktailContainer = document.getElementById("DrinkContainer") 
+        DrinkBtn.addEventListener("click",clickDrinkBtn) 
+        function clickDrinkBtn() { 
+        
+            fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php", DrinkHeaders) 
+            .then(function(response) { 
+                console.log("response is good")
+            return response.json() 
+        })
+         .then(function(data) { 
+         console.log(data)
+         const Drinkdata = data.drinks[0]
+         console.log(Drinkdata.strInstructions) 
+         const DrinkName = `<h2>${Drinkdata.strDrink}</h2>`
+         const DrinkInstruc = `<p>${Drinkdata.strInstructions}</p>`
+         const DrinkPicture = ` <img src=${Drinkdata.strDrinkThumb}> ` 
+        
+         DrinkContainer.innerHTML=DrinkName+DrinkInstruc+DrinkPicture
+    })}
