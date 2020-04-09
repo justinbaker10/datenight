1bdc8105957c238162741fbf39e332220f73d6d7
justinbaker10 <justin-baker@live.com>
2020-04-09 16:18:52 -0500

    final commit

--- js/meal.js
+++ js/meal.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
     
     
     const mealBtn = document.getElementById('wineAndDine')
-    const mealContainer = document.getElementById('mealContainer')
+    const mealContainer = document.getElementById('mealContainer2')
     mealBtn.addEventListener("click", clickMealBtn)
       function clickMealBtn () {
   
         $('#mealTitleContainer').html('')
-        $('#mealContainer').html('')
+        $('#mealContainer2').html('')
         
         fetch("https://www.themealdb.com/api/json/v1/1/random.php", mealHeaders)
         .then(function(response){
@@ -21,19 +21,17 @@ $(document).ready(function () {
           .then(function(data) {
             console.log(data,"CL Data")
             const mealData = data.meals[0]
-            /*const mealName = `<h2 id="dish">${mealData.strMeal}</h2>`
-            const mealPic = `<img src=${mealData.strMealThumb}>`
-            const mealInstruct = `<p id="instructions">${mealData.strInstructions}</p>`
-            mealContainer.innerHTML = mealName + mealPic + mealInstruct */
-            const mealCard = `<div class="card" id="drinkCard" style="width: 18rem;">
+            const mealCard = `<div class="col-sm-6" id="mealCard" style="width: 18rem;">
             <img src=${mealData.strMealThumb} class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">${mealData.strMeal}</h5>`
+              <h5 class="card-title">${mealData.strMeal}</h5>
+              <p>${mealData.strInstructions}</p>`
               
-              mealContainer.innerHTML=mealCard
+              mealContainer2.innerHTML=mealCard
             
               
           })
           
       }
   })
+  
