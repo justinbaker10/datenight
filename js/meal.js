772f79384f0781c04823c8409c60f973d9883a99
mattalagala <matt15y@gmail.com>
2020-04-09 17:08:55 -0500

    fixed alignment of Wine&Dine button

--- js/meal.js
+++ js/meal.js
@@ -1,31 +1,49 @@
 $(document).ready(function () {
 
-const mealHeaders = new Headers({
-    'x-api-key': "1"
-  })
+  const mealHeaders = new Headers({
+      'x-api-key': "1"
+    })
+    
+    
+    const mealBtn = document.getElementById('wineAndDine')
+    const mealContainer = document.getElementById('mealContainer2')
+    mealBtn.addEventListener("click", clickMealBtn)
+      function clickMealBtn () {
   
-  
-  const mealBtn = document.getElementById('wineAndDine')
-  const mealContainer = document.getElementById('mealContainer')
-  mealBtn.addEventListener("click", clickMealBtn)
-    function clickMealBtn () {
+        $('#mealTitleContainer').html('')
+        $('#drinkTitleContainer').html('')
+        $('#mealContainer').html('')
+        $('#drinkContainer').html('')
 
-      $('#mealTitleContainer').html('')
-      $('#mealContainer').html('')
-      
-      fetch("https://www.themealdb.com/api/json/v1/1/random.php", mealHeaders)
-      .then(function(response){
-          return response.json()
-           
-      })
-        .then(function(data) {
-          console.log(data,"CL Data")
-          const mealData = data.meals[0]
-          const mealName = `<h2 id="dish">${mealData.strMeal}</h2>`
-          const mealPic = `<img src=${mealData.strMealThumb}>`
-          const mealInstruct = `<p id="instructions">${mealData.strInstructions}</p>`
-          mealContainer.innerHTML = mealName + mealPic + mealInstruct        
+
+       fetch("https://www.themealdb.com/api/json/v1/1/random.php", mealHeaders)
+        .then(function(response){
+            return response.json()
+             
         })
-  
-    }
-})
+          .then(function(data) {
+            console.log(data,"CL Data")
+            const mealData = data.meals[0]
+            const mealCard = `
+            <div class='card' id="drinkCard">
+            <img src=${mealData.strMealThumb} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title" id='this'>${mealData.strMeal}</h5>
+              <h6>Try Our Receipe:</h6>
+              <p>${mealData.strInstructions}</p>
+            </div>
+            </div>`
+
+            $('#mealContainer2').html(mealCard)
+            
+            //Scroll 
+            $('html, body').animate({
+              scrollTop: $("#this").offset().top
+            }, 1000);
+            
+              
+          })
+          
+      }
+      
+  })
--- js/wiley-cocktail-api.js
+++ js/wiley-cocktail-api.js
@@ -4,24 +4,30 @@
         }); 
          
         const DrinkBtn = document.getElementById("wineAndDine") 
-        const cocktailContainer = document.getElementById("drinkContainer") 
+        const CocktailContainer = document.getElementById("mealContainer2") 
         DrinkBtn.addEventListener("click",clickDrinkBtn) 
         function clickDrinkBtn() { 
-            $('#drinkTitleContainer').html('')
-            $('#drinkContainer').html('')
         
             fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php", DrinkHeaders) 
             .then(function(response) { 
                 console.log("response is good")
             return response.json() 
         })
-         .then(function(data) { 
-         console.log(data)
-         const Drinkdata = data.drinks[0]
-         console.log(Drinkdata.strInstructions) 
-         const DrinkName = `<h2>${Drinkdata.strDrink}</h2>`
-         const DrinkInstruc = `<p>${Drinkdata.strInstructions}</p>`
-         const DrinkPicture = ` <img src=${Drinkdata.strDrinkThumb}> ` 
-        
-         cocktailContainer.innerHTML=DrinkName+DrinkPicture+DrinkInstruc
-    })}
+         .then(getDrinkData)}
+
+
+         function getDrinkData (data) { 
+            console.log(data)
+            const Drinkdata = data.drinks[0]
+            const drinkCard = `
+            <div class='card' id="drinkCard">
+            <img src=${Drinkdata.strDrinkThumb} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${Drinkdata.strDrink}</h5>
+              <h6>Mix your own drink:</h6>
+              <p>${Drinkdata.strInstructions}</p>
+            </div>
+            </div>`
+            $('#drinkContainer2').html(drinkCard)
+      
+       }
